075031ecba0cc85ef002f3ec9b7866ad16073ae9
Wilson Mbuthia <wyllyjeremy@gmail.com>
2023-12-05 12:36:48 +0300

    Initial commit

---
+++ Forminator-Plugin-Validations.php
@@ -0,0 +1,20 @@
+/**
+* Forminator Plugin Validations
+*
+* @author The Jitu
+* @copyright 2019 The Jitu
+* @license GPL-2.0-or-later
+*
+* @wordpress-plugin
+* Plugin Name: Forminator Plugin Validations
+* Plugin URI: https://github.com/Wyllymk/Forminator-Plugin-Validations
+* Description: Plugin which adds validation to forminator fields.
+* Version: 1.0.0
+* Requires at least: 5.2
+* Requires PHP: 7.2
+* Author: The Jitu
+* Author URI: https://thejitu.com
+* Text Domain: forminator-plugin-validations
+* License: GPL v2 or later
+* License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+*/
